51923ba8cd03211f1e2ac1c5ae4ad4d99636b234
Daniel Leushuis <danieleushuis@gmail.com>
2024-07-28 15:13:57 +0200

    navigate pages

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PopperDirective } from '../../directives/popper-directive';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { FireBaseProvider } from '../../../providers/firebase.provider';
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
     selector: 'app-header',
@@ -15,16 +17,23 @@ import { FireBaseProvider } from '../../../providers/firebase.provider';
 })
 export class HeaderComponent implements OnInit {
     email: string = '';
+    currentUrl: string = null;
 
     constructor(
-        private fireBaseProvider: FireBaseProvider
+        private fireBaseProvider: FireBaseProvider,
+        private router: Router
     ) { }
 
     async ngOnInit(): Promise<void> {
+        this.currentUrl = this.router.url;
         this.email = (await this.fireBaseProvider.user)?.email ?? '';
     }
 
     async logout() {
         await this.fireBaseProvider.logout();
+    }
+
+    navigateTo(path: string) {
+        this.router.navigate([path]);
     }
 }
